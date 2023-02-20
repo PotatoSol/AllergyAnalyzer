2f659a90838bf03a95540c31ab40a46794286296
Michael Sol <sol.mike.j@gmail.com>
2023-02-20 15:53:58 -0800

    add files
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>

---
+++ AllergyAnalyzer.Tests/ModelTests/AllergyAnalyzerTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AllergyAnalyzer;
+
+namespace AllergyAnalyzer.Tests
+{
+  [TestClass]
+  public class AllergyAnalyzerTests
+  {
+    [TestMethod]
+    public void QueenIsInRange_TestsIfTargetIsInRange_CorrectlyFindsRange()
+    {
+      
+      Assert.AreEqual(true, true);
+    }
+  }
+}
